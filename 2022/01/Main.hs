9305a152fdc92c2e44afd4d975c261b993a796d1
Regis Kuckaertz <regis.kuckaertz@pirum.com>
2022-12-02 18:11:07 +0000

    Step 2

--- 2022/01/Main.hs
+++ 2022/01/Main.hs
@@ -11,10 +11,14 @@ type Calories = Int
 count_calories :: Elf -> Calories
 count_calories = sum
 
-most_calories :: [Calories] -> Calories
-most_calories = maximum
+most_calories :: [Calories] -> (Calories, Calories, Calories)
+most_calories = foldr merge (0, 0, 0)
+  where merge x (a, b, c) | x > a = (x, a, b)
+                          | x > b = (a, x, b)
+                          | x > c = (a, b, x)
+                          | otherwise = (a, b, c)
 
-exercise :: [Elf] -> Calories
+exercise :: [Elf] -> (Calories, Calories, Calories)
 exercise = most_calories . map count_calories
 
 calories :: Parser Calories
@@ -30,4 +34,8 @@ main = do
   parseFromFile elves "input.txt" >>= \result ->
     case result of
       Left err -> print err
-      Right elves -> print (exercise elves)
+      Right elves -> 
+        let (x, y, z) = exercise elves
+        in do
+          print (x, y, z)
+          print (x + y + z)
